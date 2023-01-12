c7e24decceb76cab2d690aa41c27ae9b59034fe3
Ankush8950 <69650280+Ankush8950@users.noreply.github.com>
2023-01-12 23:57:42 +0530

    update

--- src/components/FilterBtn.js
+++ src/components/FilterBtn.js
@@ -6,7 +6,7 @@ const FilterBtn = ({ filterItems}) => {
       <div className="mt-5 pl-0 md:pl-20 mb-3 text-center">
         <button
           className="bg-[#1f242c] text-white text-sm p-2 m-1 sm:m-2 rounded-lg hover:bg-white hover:text-[#1f242c] border-2"
-          onClick={() => filterItems("cloth")}
+          onClick={() => filterItems("Clothes")}
         >
           Clothes
         </button>
@@ -18,13 +18,13 @@ const FilterBtn = ({ filterItems}) => {
         </button>
         <button
           className="bg-[#1f242c] text-white text-sm p-2 m-2 rounded-lg hover:bg-white hover:text-[#1f242c] border-2"
-          onClick={() => filterItems("Furniture")}
+          onClick={() => filterItems("Titulo 3 Cambiado")}
         >
           Furniture
         </button>
         <button
           className="bg-[#1f242c] text-white text-sm p-2 m-2 rounded-lg hover:bg-white hover:text-[#1f242c] border-2"
-          onClick={() => filterItems("Electronics")}
+          onClick={() => filterItems("electronics test")}
         >
           Electronics
         </button>
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
   const [data, setData] = useState([]);
   const [filterdata, setFilterData] = useState(data);
   const [addproduct, setAddProduct] = useState([]);
-  console.log(data);
+  console.log(data)
   // Api call
   // https://api.escuelajs.co/api/v1/products
   const url = "https://api.escuelajs.co/api/v1/products";
