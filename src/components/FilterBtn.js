548363df415b2b671b45e45068d806dbb70e087a
Ankush8950 <69650280+Ankush8950@users.noreply.github.com>
2023-01-16 11:52:33 +0530

    update category

--- src/components/FilterBtn.js
+++ src/components/FilterBtn.js
@@ -6,7 +6,7 @@ const FilterBtn = ({ filterItems}) => {
       <div className="mt-5 pl-0 md:pl-20 mb-3 text-center">
         <button
           className="bg-[#1f242c] text-white text-sm p-2 m-1 sm:m-2 rounded-lg hover:bg-white hover:text-[#1f242c] border-2"
-          onClick={() => filterItems("Man's SHOES")}
+          onClick={() => filterItems("Clothes")}
         >
           Clothes
         </button>
